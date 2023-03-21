50280fb0ef538885ffaa2df6e87a5264c93a7273
Martin Holst Swende <martin@swende.se>
2023-03-21 17:04:26 +0900

    params: begin v1.11.6 release cycle

--- params/version.go
+++ params/version.go
@@ -22,10 +22,10 @@ import (
 
 const (
 	// Version is the version of upstream geth
-	VersionMajor = 1        // Major version component of the current release
-	VersionMinor = 11       // Minor version component of the current release
-	VersionPatch = 5        // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionMajor = 1          // Major version component of the current release
+	VersionMinor = 11         // Minor version component of the current release
+	VersionPatch = 6          // Patch version component of the current release
+	VersionMeta  = "unstable" // Version metadata to append to the version string
 
 	// KromaVersion is the version of kroma-geth
 	KromaVersionMajor = 0          // Major version component of the current release
